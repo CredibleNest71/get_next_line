0b2de1611335395711d1199fd7b328f40d990ca4
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2023-09-26 16:35:19 +0000

    DRAFT

---
+++ get_the_nextest_line.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_the_nextest_line.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 16:26:53 by codespace         #+#    #+#             */
+/*   Updated: 2023/09/26 16:35:06 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+    char	*line;
+    char	rest[BUFFER_SIZE];
+	char	*buf;
+	int		
+
+
+	if (//REST NOT EMPTY)
+		line //APPEND REST
+
+	buf = //read_into_buf
+	
+	while (//END NOT FOUND in buf)
+	{
+
+		line  //APPEND BUF
+		buf = //read into buf
+		check_buf_for_end
+	}
+	line //append 
+	rest //set rest
+	return line 
+}
