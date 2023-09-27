9518ab2848e4bb2c43f61269fa6d361d485e3f26
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2023-09-27 09:38:32 +0000

    workin

--- get_the_nextest_line.c
+++ get_the_nextest_line.c
@@ -6,33 +6,149 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 16:26:53 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/26 16:35:06 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/27 09:37:36 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+size_t	len(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*expand(char *old, size_t size)
+{
+	size_t	i;
+	size_t	newlength;
+	char	*new;
+	
+	newlength = len(old) + size + 1;
+	i = 0;
+	new = (char *) malloc(newlength);
+	while(i < newlength)
+		new[i++] = 0;
+	i = 0;
+	while (old[i])
+	{
+		new[i] = old[i];
+		i++;
+	}
+	free(old);
+	return(new);
+}
+
+void	append(char *dst, char *src, int size)
+{
+	int	i;
+	int	length;
+
+	length = len(dst);
+	i = 0;
+	while (src[i] && i < size)
+	{
+		dst[length + i] = src[i];
+		i++;
+	}
+	dst[length + i] = 0;
+}
+
+void	set_rest(char *rest, char *src, int bookmark)
+{
+	int	i;
+
+	i = 0;
+	while (i < BUFFER_SIZE)
+		rest[i++] = 0;
+	i = 0;
+	while (bookmark < BUFFER_SIZE)
+		rest[i++] = src[bookmark++];
+}
+
+char	*get_buf(int fd)
+{
+	char	*buf;
+	int		bytesread;
+	
+	buf = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return(NULL);
+	bytesread = read(fd, buf, BUFFER_SIZE);
+	if (bytesread <= 0)
+		return (NULL);
+	buf[BUFFER_SIZE] = 0;
+	return (buf);
+}
+
+int		check_buf(char *buf)
+{
+	int	i;
+
+	i = 0;
+	while (buf[i])
+	{
+		if (buf[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
 
 char	*get_next_line(int fd)
 {
-    char	*line;
-    char	rest[BUFFER_SIZE];
-	char	*buf;
-	int		
+    char		*line;
+    static char	rest[BUFFER_SIZE];
+	char		*buf;
+	int			bookmark;
 
-
-	if (//REST NOT EMPTY)
-		line //APPEND REST
-
-	buf = //read_into_buf
-	
-	while (//END NOT FOUND in buf)
+	line = (char *) malloc(BUFFER_SIZE);
+	append(line, rest, BUFFER_SIZE);
+	while (1)
 	{
-
-		line  //APPEND BUF
-		buf = //read into buf
-		check_buf_for_end
+		buf = get_buf(fd);
+		bookmark = check_buf(buf);
+		if (bookmark > 0)
+			break;
+		line = expand(line, BUFFER_SIZE);
+		append(line, buf, BUFFER_SIZE);
+		free(buf);
 	}
-	line //append 
-	rest //set rest
-	return line 
+	line = expand(line, bookmark);
+	append(line, buf, bookmark);
+	set_rest(rest, buf, bookmark);
+	free(buf);
+	return (line);
+}
+
+int	main(void)
+{
+	char	*line;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+	fd1 = open("tests/test.txt", O_RDONLY);
+	fd2 = open("tests/test2.txt", O_RDONLY);
+	fd3 = open("tests/test3.txt", O_RDONLY);
+	line = get_next_line(fd1);
+	printf("%s", line);
+	line = get_next_line(fd1);
+	printf("%s", line);	
+	line = get_next_line(fd1);
+	printf("%s", line);	
+	line = get_next_line(fd1);
+	printf("%s", line);	
+	line = get_next_line(fd1);
+	printf("%s", line);
+	free(line);
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	return (0);
 }
